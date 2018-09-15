338db582a12f62cf7201f687aab311dcbd331dac
bm295 <baominh.nguyen.295@gmail.com>
2018-09-15 23:24:06 +0700

    add homeController

---
+++ controller/home-controller.js
@@ -0,0 +1,5 @@
+const getHomeView = function(request, response) {
+  return response.render("home")
+}
+
+exports.getHomeView = getHomeView
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express')
 var app = express()
+var homeController = require('./controller/home-controller')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.get('/', function(request, response) {
-  response.render("home")
-})
+app.get('/', homeController.getHomeView)
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
