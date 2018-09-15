800718d3b1f6de8b39712d73b228020c0235e78b
bm295 <baominh.nguyen.295@gmail.com>
2018-09-15 23:07:05 +0700

    add ejs

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-/* var express = require("express");
-var app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set("views", "./views");
-app.listen(5000);
-
-app.get("/", function(req, res) {
-  res.render("home");
-}); */
-
 var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.get('/', function(request, response) {
-  response.send('Trying to build my kingdom! A farmer here!')
+  response.render("home")
 })
 
 app.listen(app.get('port'), function() {
