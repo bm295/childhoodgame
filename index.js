fdeb5b5ded1c84b4b241fa78f885ff9e64e72c77
bm295 <baominh.nguyen.295@gmail.com>
2018-09-15 22:30:32 +0700

    new approach

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require("express");
+/* var express = require("express");
 var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -7,4 +7,18 @@ app.listen(5000);
 
 app.get("/", function(req, res) {
   res.render("home");
-});
+}); */
+
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Trying to build my kingdom! A farmer here!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
