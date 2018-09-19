021b40348e6bb54b440b21b958e303ee8b77dcfe
bm295 <baominh.nguyen.295@gmail.com>
2018-09-19 15:55:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,43 @@ app.use(express.static(__dirname + '/public'))
 app.set("view engine", "ejs")
 app.set("views", "./views")
 
-app.get('/', homeController.getHomeView)
+/* app.get('/', homeController.getHomeView) */
+
+const pg = require('pg')
+const config = {
+  user: 'bcfodeyhmhsrcn',
+  host: 'ec2-54-197-230-161.compute-1.amazonaws.com',
+  database: 'd9lmukb58d9f3g',
+  password: '39021d98ad2a8aeaffda294b550bd73f560e423afba332b26e7c925dcf846a21',
+  port: 5432,
+  ssl: true,
+}
+const pool = new pg.Pool(config)
+
+app.get('/', function(request, response) {
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err)
+    }
+    
+    client.query('SELECT "Id", "RoleId", "Level" FROM "Person";', function(err, result) {
+      done()
+      
+      if(err) {
+        response.end()
+        return console.error('error running query', err)
+      }
+      
+      response.render("home", { data: result })
+    })
+  })  
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-const { Client } = require('pg')
+/* const { Client } = require('pg')
 
 const client = new Client({
   user: 'bcfodeyhmhsrcn',
@@ -32,4 +62,4 @@ client.query('SELECT "Id", "RoleId", "Level" FROM "Person";', (err, res) => {
     console.log(JSON.stringify(row))
   }
   client.end()
-})
+}) */
